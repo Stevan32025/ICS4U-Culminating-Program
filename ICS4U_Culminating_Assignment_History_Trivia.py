c9fcff12278d64bb2f593859b33c0d27df295c89
Stevan32025 <50918931+Stevan32025@users.noreply.github.com>
2019-05-28 16:35:08 -0400

    Add files via upload

---
+++ ICS4U_Culminating_Assignment_History_Trivia.py
@@ -0,0 +1,6 @@
+# -------------------------------------------------------------------------------
+# Name: <ICS4U_Culminating_Assignment_History_Trivia>.py
+# Purpose: <This trivia game program will quiz people on three subjects, with 99 questions in total. Points are earned for each correct answer. 
+# Author: <Majlovic>.<S>, <Hajiyev>.<I>, <Rogers>.<S>, <Rakov>.<P> 
+# Created: <28/05/2019>
+# ------------------------------------------------------------------------------

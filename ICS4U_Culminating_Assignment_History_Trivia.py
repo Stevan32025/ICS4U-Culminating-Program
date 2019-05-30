230d6681acd8e5b34d5102c5d0311725cfd35519
Stevan32025 <50918931+Stevan32025@users.noreply.github.com>
2019-05-30 16:15:19 -0400

    Delete ICS4U_Culminating_Assignment_History_Trivia.py

--- ICS4U_Culminating_Assignment_History_Trivia.py
+++
@@ -1,6 +0,0 @@
-# -------------------------------------------------------------------------------
-# Name: <ICS4U_Culminating_Assignment_History_Trivia>.py
-# Purpose: <This trivia game program will quiz people on three subjects, with 99 questions in total. Points are earned for each correct answer. 
-# Author: <Majlovic>.<S>, <Hajiyev>.<I>, <Rogers>.<S>, <Rakov>.<P> 
-# Created: <28/05/2019>
-# ------------------------------------------------------------------------------
